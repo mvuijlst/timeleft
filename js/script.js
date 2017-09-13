3ba2c40ef7dc8119c7a0b547991a0cb3f27478ea
Michel Vuijlsteke from Cthulhu <michel@zog.org>
2017-09-13 18:31:11 +0200

    wat opgekuist

--- js/script.js
+++ js/script.js
@@ -32,8 +32,7 @@ function startTime() {
     var endOfYear = new Date(yy + 1, 1, 1, 0, 0, 0, 0) - 1;
     var pensionDate = new Date(birthDate.getFullYear() + pensionAge, birthDate.getMonth(), birthDate.getDate());
 
-    $("#now").html(dd + "/" + mm + "/" + yy + " " + h + ":" + doPad(m, 2) + ":" + doPad(s, 2) + "." + doPad(ms, 3));
-
+    $('#now').html('Het is ' + fuzzyTime(h, m) + ' op ' + dateFormat(now) + '.<br/> Nog ' + parseInt(daysBetween(now, pensionDate)) + ' dagen tot uw pensioen.');
     makeBar('milliseconds2', 'Seconde', 0, 1000, ms, '', '', 'ms', 'square');
     makeBar('seconds', 'Minuut', 0, toMs(0, 1), toMs(0, 0, s, ms), '', '', 's.ms', 'none');
     makeBar('minutes', 'Uur', 0, toMs(1, 0), toMs(0, m, s, ms), '', '', 'm:s', 'none');
@@ -110,7 +109,7 @@ function makeBar(id, label, start, end, now, startLabel, endLabel, format, ease,
         $(thisID + ' .percentagedone').css('width', percentagedone);
         $(thisID + ' .label .right').text(donelabel);
     } else {
-        bar = '<div id="' + id + '"></div>';
+        bar = '<div id="' + id + '" class="progress"></div>';
         $('#content').append(bar);
         $(thisID).append('<div class="label"><div class="left">' + label + '</div><div class="right">' + donelabel + '</div>');
         $(thisID).append('<div class="total"><div class="percentagedone"></div></div></div>');
@@ -178,14 +177,60 @@ function formatDoneLabel(format, end, start, now, precision) {
     return donelabel;
 }
 
+
+function fuzzyTime(h, m) {
+    hours = ['middernacht', 'één', 'twee', 'drie', 'vier', 'vijf', 'zes', 'zeven', 'acht', 'negen', 'tien', 'elf', 'twaalf', 'één', 'twee', 'drie', 'vier', 'vijf', 'zes', 'zeven', 'acht', 'negen', 'tien', 'elf', 'middernacht'];
+    if (m < 30) {
+        x = hours[h];
+    } else {
+        x = hours[h + 1];
+    }
+    if (m == 59 || m == 0) { x += ' uur' };
+    if (m == 0) { return ('exact ' + x); }
+    if (m >= 1 && m <= 4) { return ('een beetje na ' + x); }
+    if (m == 5) { return ('vijf na ' + x); }
+    if (m >= 6 && m <= 9) { return ('bijna tien na ' + x); }
+    if (m == 10) { return ('tien na ' + x); }
+    if (m >= 11 && m <= 14) { return ('bijna kwart na ' + x); }
+    if (m == 15) { return ('kwart na ' + x); }
+    if (m >= 16 && m <= 17) { return ('kwart na ' + x + ' en een beetje'); }
+    if (m >= 18 && m <= 19) { return ('bijna twintig na ' + x); }
+    if (m == 20) { return ('twintig na ' + x); }
+    if (m >= 21 && m <= 29) { return ('bijna half ' + x); }
+    if (m == 30) { return ('half ' + x); }
+    if (m >= 30 && m <= 35) { return ('een beetje na half ' + x); }
+    if (m >= 36 && m <= 39) { return ('bijna twintig voor ' + x); }
+    if (m == 40) { return ('twintig voor ' + x); }
+    if (m >= 41 && m <= 44) { return ('ongeveer kwart voor ' + x); }
+    if (m == 45) { return ('kwart voor ' + x); }
+    if (m >= 46 && m <= 49) { return ('bijna tien voor ' + x); }
+    if (m == 50) { return ('tien voor ' + x); }
+    if (m >= 51 && m <= 54) { return ('iets na tien voor ' + x); }
+    if (m == 55) { return ('vijf voor ' + x); }
+    if (m >= 56 && m <= 59) { return ('bijna ' + x); }
+}
+
+function dateFormat(date) {
+    var montharray = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
+    var dayarray = new Array('zondag', 'maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag');
+    return (dayarray[date.getDay()] + ' ' + date.getDate() + ' ' + montharray[date.getMonth()] + ' ' + date.getFullYear());
+}
+
 function daysInMonth(date) {
     return new Date(date.getYear(),
         date.getMonth() + 1,
         0).getDate();
 }
 
-function doPad(i, ln) {
-    return i.toString().length < ln ? "0".repeat(ln - i.toString().length) + i : i;
+function treatAsUTC(date) {
+    var result = new Date(date);
+    result.setMinutes(result.getMinutes() - result.getTimezoneOffset());
+    return result;
+}
+
+function daysBetween(startDate, endDate) {
+    var millisecondsPerDay = 24 * 60 * 60 * 1000;
+    return (treatAsUTC(endDate) - treatAsUTC(startDate)) / millisecondsPerDay;
 }
 
 function toMs(h, m, s, ms) {
@@ -195,6 +240,10 @@ function toMs(h, m, s, ms) {
     ret += m * 60 * 1000;
     ret += h * 60 * 60 * 1000;
     return ret;
+}
+
+function doPad(i, ln) {
+    return i.toString().length < ln ? "0".repeat(ln - i.toString().length) + i : i;
 }
 
 function checkPlural(options, nbr) {
